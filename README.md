fe236633251fa3c792f858540737d01549a42619
lightbitbird <maxgramf3@gmail.com>
2019-05-06 16:25:14 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Django App
+====
+
+## Description
+Django Application learning from [Django Girls Tutorial](https://tutorial.djangogirls.org/en/)
+
+
+## How to Use
+Access http://127.0.0.1:8000 after following setup.
+
+```
+$ cd djaongo-app
+$ python3 -m venv env
+$ source env/bin/activate
+$ pip install -r requirements.txt 
+$ python manag.py migrate
+$ python manage.py createsuperuser
+$ python manage.py runserver
+```
---
+++ app/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    """Book"""
+    name = models.CharField('書籍名', max_length=255)
+    publisher = models.CharField('出版社', max_length=255, blank=True)
+    page = models.IntegerField('ページ数', blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Impression(models.Model):
+    """Impression"""
+    book = models.ForeignKey(Book, verbose_name='書籍', related_name='impressions', on_delete=models.CASCADE)
+    comment = models.TextField('コメント', blank=True)
+
+    def __str__(self):
+        return self.comment
+
